d18e6adbb1fb304e52901bad9790ce75e9ae6d43
DevFernanda <mattiello.developer@gmail.com>
2022-12-08 10:45:19 -0300

    Finalização do projeto, inclusão de rotas e API para requisições externas

--- src/app/novaTransferencia/nova-transferencia.component.ts
+++ src/app/novaTransferencia/nova-transferencia.component.ts
@@ -1,4 +1,8 @@
+import { Transferencia } from './../models/transferencias.model';
+import { TransferenciaService } from './../services/transferencia.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from '@angular/router'
 
 @Component ({
     selector: 'app-nova-transferencia',
@@ -14,12 +18,21 @@ import { Component, EventEmitter, Output } from "@angular/core";
     valor: number;
     destino: number;
 
+    constructor(private service: TransferenciaService, private router: Router) {
+
+    }
+
     transferir() {
       console.log('Solicitado nova transferência');
-      const valorEmitir = { valor: this.valor, destino: this.destino };
-      this.aoTransferir.emit(valorEmitir)
 
-      this.limparCampos();
+      const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
+
+      this.service.adicionar(valorEmitir).subscribe(resultado => {
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato')
+      },
+      error => console.error(error));
     }
 
     limparCampos(){
